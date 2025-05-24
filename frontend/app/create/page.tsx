2eac95fe10c1dad245407c4f567e96a4df85d623
mbcse <mbcse50@gmail.com>
2025-05-24 13:35:20 +0530

    fix

--- frontend/app/create/page.tsx
+++ frontend/app/create/page.tsx
@@ -1,8 +1,5 @@
 import { SessionProvider } from '../context/SessionContext';
-import AgentCreationPage from '../components/AgentCreationPage';
 
 export default function CreatePage() {
-  return (
-      <AgentCreationPage />
-  );
+  
 } 
